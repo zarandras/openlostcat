a5256c96290ccb3d454976446b746fe513bc4a85
lukacsg <lukacs.hod@gmail.com>
2021-02-22 16:19:03 +0100

    ANY and ALL to string function update: the nmae is included to string

--- openlostcat/operators/quantifier_operators.py
+++ openlostcat/operators/quantifier_operators.py
@@ -11,20 +11,21 @@ class ALL(AbstractBoolOperator):
 
     """
 
-    str_template = "ALL(\n{operator}\n)"
+    str_template = "ALL[{name}](\n{operator}\n)"
             
     def __init__(self, name, operator):
         if self.is_bool_op(operator):
             error("ALL is not defined for bool operators", operator)
         self.filter_operator = operator
-        self.name = self.get_name("__ALL", name, self.filter_operator)
+        self.name = self.get_name("__ALL_", name, self.filter_operator)
 
     def apply(self, tag_bundle_set):
         matching_tag_bundles = self.filter_operator.apply(tag_bundle_set)
         return len(matching_tag_bundles) == len(tag_bundle_set), [(self.name, matching_tag_bundles)]
     
     def __str__(self):
-        return self.str_template.format(operator=indent(str(self.filter_operator), base_indent_num))
+        return self.str_template.format(name=self.name[6:],
+                                        operator=indent(str(self.filter_operator), base_indent_num))
 
 
 class ANY(AbstractBoolOperator):
@@ -32,17 +33,18 @@ class ANY(AbstractBoolOperator):
 
     """
     
-    str_template = "ANY(\n{operator}\n)"
+    str_template = "ANY[{name}](\n{operator}\n)"
             
     def __init__(self, name, operator):
         if self.is_bool_op(operator):
             error("ANY is not defined for bool operators", operator)
         self.filter_operator = operator
-        self.name = self.get_name("__ANY", name, self.filter_operator)
+        self.name = self.get_name("__ANY_", name, self.filter_operator)
   
     def apply(self, tag_bundle_set):
         matching_tag_bundles = self.filter_operator.apply(tag_bundle_set)
         return len(matching_tag_bundles) > 0, [(self.name, matching_tag_bundles)]
-    
+
     def __str__(self):
-        return self.str_template.format(operator=indent(str(self.filter_operator), base_indent_num))
+        return self.str_template.format(name=self.name[6:],
+                                        operator=indent(str(self.filter_operator), base_indent_num))
