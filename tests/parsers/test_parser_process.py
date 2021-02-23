20277d8651523e918f650664720960d8e7c64211
lukacsg <lukacs.hod@gamil.com>
2021-02-23 17:07:03 +0100

    Update test

--- tests/parsers/test_parser_process.py
+++ tests/parsers/test_parser_process.py
@@ -7,18 +7,26 @@ from openlostcat.parsers.opexpressionparser import OpExpressionParser
 from openlostcat.parsers.categoryorrefdefparser import CategoryOrRefDefParser
 from openlostcat.parsers.categorycatalogparser import CategoryOrRefDefParser
 
+
+from unittest.mock import patch
 from unittest.mock import MagicMock
 
 
 class TestParserProcess(unittest.TestCase):
 
-    def test_simplify_operator(self):
+    @patch('openlostcat.parsers.opexpressionparser.ANY.get_name')
+    def test_simplify_operator(self, get_name_mock):
         """Test the simplify operator functionality:
         If a bool AND or OR contain only 1 operator, than it will optimizes the parser and leave it out.
         """
-        oparator_json = {"__OR_": [{"__AND_": {"__ANY_": {"__FilterCONST_": True}}}]}
+        get_name_mock.return_value = '__ANY_test'
+        oparator_json = {"__OR_": [{"__AND_": {"__OR_": [{"__AND_": {"__ANY_prototype": {"__FilterCONST_": True}}}]}}]}
         op_parser = OpExpressionParser()
-        self.assertEqual(str(op_parser.parse(oparator_json)).replace(" ", "").replace("\n", " "), "test")
+        self.assertEqual("ANY[test]( const(True) )",
+                         str(op_parser.parse(oparator_json)).replace(" ", "").replace("\n", " "))
+
+
+
 
 
 if __name__ == '__main__':
